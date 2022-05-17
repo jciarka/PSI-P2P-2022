159a2ad4a3aa58decd0b5ac9f44901c9b25d42a2
Jakub Ciarka <01104656@pw.edu.pl>
2022-05-18 00:12:06 +0200

    main file refactoring

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-p", "--path",
                     default=DEFAULT_RESOURCE_PATH,
                     help="path to resources that uou want to share")
-parser.add_argument("-e", "--expiration",
-                    type=int,
-                    default=120,
-                    help="Time in seconds that gathered info is teated as valid"
-                    )
 parser.add_argument("-d", "--delay",
                     type=int,
                     default=2,
                     help="Time in seconds that cilient will wait for serwer answers")
+parser.add_argument("-a", "--awake_period",
+                    type=int,
+                    default=CATALOG_PUBLISHER_AWAKE_PERIOD,
+                    help="Publisher awake period in seconds")
+parser.add_argument("-s", "--publish_period",
+                    type=int,
+                    default=CATALOG_PUBLISHER_SEND_PERIOD,
+                    help="Publishing period in second")
 parser.add_argument("-g", "--group",
                     type=int,
                     default=RESOURCE_GROUP_ID,
@@ -35,29 +38,33 @@ parser.add_argument("-g", "--group",
 args = parser.parse_args()
 
 # set up resources and request data
-client = Client(args.delay, VERSION, RESOURCE_GROUP_ID)
+client = Client(args.delay, VERSION, args.group)
 resources = Resources()
 get_all_resources(client, args.path, resources)
 
 # set up catalog service
 catalog_server = CatalogService(resources, args.path)
+
 catalog_server_thread = threading.Thread(
     target=catalog_server.run)
+
 catalog_server_thread.start()
 
 # set up catalog publisher
 catalog_publisher = CatalogPublisher(
     VERSION,
     args.path,
-    RESOURCE_GROUP_ID,
-    CATALOG_PUBLISHER_AWAKE_PERIOD,
-    CATALOG_PUBLISHER_SEND_PERIOD)
+    args.group,
+    args.awake_period,
+    args.publish_period)
+
 catalog_publisher_thread = threading.Thread(
     target=catalog_publisher.run)
+
 catalog_publisher_thread.start()
 
-# set up transfer service
-# TO DO:
+# TO DO: set up transfer service
+
 
 # command loop
 while True:
@@ -81,7 +88,7 @@ while True:
 # free services and publisher
 catalog_server.cancelation_token = True
 catalog_publisher.cancelation_token = True
-print("Turning of services...")
+print("Turning off services...")
 
 catalog_server_thread.join()
 catalog_publisher_thread.join()
