a69725dbff3185feed057511ae139873c1405284
Michał Ciborowski <mciborowski.it@gmail.com>
2022-06-02 17:39:00 +0200

    Dodano PUSH

--- commands/push.py
+++ commands/push.py
@@ -1,5 +1,9 @@
 import argparse
 from commands.errors import InvalidCommandError
+from config import DEFAULT_RESOURCE_PATH, LOCAL_RESOURCE_ADDRESS
+from infrastructure.resources import Resources
+from infrastructure.resource import Resource
+import shutil
 
 
 class Push:
@@ -18,8 +22,7 @@ class Push:
 
         parser.add_argument(
             "-r", "--resource", nargs='*', type=str, required=True)
-        parser.add_argument(
-            "-d", "--directory", type=list, required=True)
+
 
         return parser
 
@@ -27,5 +30,26 @@ class Push:
     def print_usage():
         return Push.__getParser().print_help()
 
-    def execute(self, client):
-        raise NotImplementedError()
+    def execute(self, client, resources: Resources,
+                resource_path=DEFAULT_RESOURCE_PATH):
+        try:
+            shutil.copy2(self.args.resource[0], resource_path)
+            print("File pushed successfully.")
+
+        except IndexError:
+            print("Invalid name")
+        # If source and destination are same
+        except shutil.SameFileError:
+            print("Source and destination represents the same file.")
+
+        # If destination is a directory.
+        except IsADirectoryError:
+            print("Destination is a directory.")
+
+        # If there is any permission issue
+        except PermissionError:
+            print("Permission denied.")
+
+        # For other errors
+        except:
+            print("Error occurred while pushing file.")
