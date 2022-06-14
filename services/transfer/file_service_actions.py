6e99afa62d2ca8f11aa584ada07eeb88a32d2f6e
quechulo <aawangardowy@gmail.com>
2022-06-14 21:01:31 +0200

    added

--- services/transfer/file_service_actions.py
+++ services/transfer/file_service_actions.py
@@ -10,6 +10,26 @@ class SendFileAction:
         self.__address = address
 
     def execute(self):
+        s = socket.socket()         # Create a socket object
+        host = socket.gethostname() # Get local machine name
+        port = 12345                 # Reserve a port for your service.
+        s.bind((host, port))        # Bind to the port
+        desired_file = '' ## TODO desired file name
+        f = open(desired_file,'wb')
+        s.listen(5)                 # Now wait for client connection.
+        while True:
+            c, addr = s.accept()     # Establish connection with client.
+            print 'Got connection from', addr
+            print "Receiving..."
+            l = c.recv(1024)
+            while (l):
+                print "Receiving..."
+                f.write(l)
+                l = c.recv(1024)
+            f.close()
+            print "Done Receiving"
+            c.send('Thank you for connecting')
+            c.close()                # Close the connection
         # serializer = InjectionContainer["serializer"]
         # data = serializer.deserialzie(body)
 
