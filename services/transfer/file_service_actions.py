123fbd3132f8247052ecbcf8950605b8928a0d15
Jakub Ciarka <01104656@pw.edu.pl>
2022-06-14 21:52:28 +0200

    File service handler 1 version

--- services/transfer/file_service_actions.py
+++ services/transfer/file_service_actions.py
@@ -1,39 +1,71 @@
 # from os import listdir
 # from os.path import isfile, join, getmtime
-from config import InjectionContainer, FILE_TRANSFER_PORT
-# from infrastructure.local_resources_util import LocalResourcesUtil
+import socket
+
+from config import \
+    VERSION,\
+    FILE_SERVICE_ONE_READ_LEN,\
+    RESOURCE_GROUP_ID,\
+    InjectionContainer,\
+    FILE_TRANSFER_PORT,\
+    RESPONSE_STATUSES,\
+    FILE_SERVICE_REQUEST_HEADER_LEN
+
+from infrastructure.catalog_messages_util import \
+    CatalogMessagesUtil
+
+
+from infrastructure.catalog_messages_errors import \
+    StatusCodeException
 
 
 class SendFileAction:
     def __init__(self, connection, address) -> None:
-        self.__connection = connection,
+        self.__socket = connection,
         self.__address = address
 
     def execute(self):
-        s = socket.socket()         # Create a socket object
-        host = socket.gethostname() # Get local machine name
-        port = 12345                 # Reserve a port for your service.
-        s.bind((host, port))        # Bind to the port
-        desired_file = '' ## TODO desired file name
-        f = open(desired_file,'wb')
-        s.listen(5)                 # Now wait for client connection.
-        while True:
-            c, addr = s.accept()     # Establish connection with client.
-            print 'Got connection from', addr
-            print "Receiving..."
-            l = c.recv(1024)
-            while (l):
-                print "Receiving..."
-                f.write(l)
-                l = c.recv(1024)
-            f.close()
-            print "Done Receiving"
-            c.send('Thank you for connecting')
-            c.close()                # Close the connection
-        # serializer = InjectionContainer["serializer"]
-        # data = serializer.deserialzie(body)
+        try:
+            body = ""
+            header = self.__socket.recv(FILE_SERVICE_REQUEST_HEADER_LEN)
 
-        # TODO: implement response for send file requirement
+            _, _, _, group_id, msg_len = \
+                CatalogMessagesUtil.parse_file_request_header(
+                    header, VERSION, RESOURCE_GROUP_ID)
 
-        pass
-        # wywłanie pliku
+            while len(body < msg_len):
+                body += self.__socket.recv(FILE_SERVICE_ONE_READ_LEN)
+
+            serializer = InjectionContainer['serializer']
+            file_name = serializer.deserialzie(body)['filename']
+
+            with open(file_name, 'wb') as f:
+                file_data = f.read()
+
+            response = CatalogMessagesUtil.generate_file_response(
+                    VERSION, 0, RESPONSE_STATUSES.SUCCESS.value,
+                    group_id, file_data)
+
+            self.__socket.sendall(response)
+
+        except OSError:
+            response = CatalogMessagesUtil.generate_file_response(
+                VERSION, 0, RESPONSE_STATUSES.NOT_FOUND.value, group_id)
+
+            self.__socket.sendto(response)
+            return
+
+        except StatusCodeException as ex:
+            response = CatalogMessagesUtil.generate_file_response(
+                VERSION, 0, ex.GetStatusCode().value,
+                ex.get_group_id)
+
+            self.__socket.sendall(response)
+            return
+
+        except (socket.timeout, Exception):
+            response = CatalogMessagesUtil.generate_file_response(
+                VERSION, 0, RESPONSE_STATUSES.SERVER_ERROR.value, group_id)
+
+            self.__socket.sendto(response)
+            return
